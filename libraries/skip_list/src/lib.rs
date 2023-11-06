a9685310b2b08fe78fad0ef8b7f5670d09d67e6f
Luka <luka.lukas159@gmail.com>
2023-11-06 22:04:56 +0100

    Making SkipList available for arbitary key and value lengths, structure not complete

--- libraries/skip_list/src/lib.rs
+++ libraries/skip_list/src/lib.rs
@@ -1,6 +1,6 @@
 mod skip_list;
 
-pub use skip_list::SkipList
+pub use skip_list::SkipList;
 
 #[cfg(test)]
 mod tests {
@@ -10,67 +10,88 @@ mod tests {
     fn test_insert_and_get() {
         let mut skip_list = SkipList::new(4);
 
-        let key1 = [1, 1, 1, 1, 1, 1, 1, 1];
-        let value1 = [10, 10, 10, 10, 10, 10, 10, 10];
+        skip_list.insert(&[1], &[10]);
+        skip_list.insert(&[2], &[20]);
+        skip_list.insert(&[3], &[30]);
 
-        let key2 = [2, 2, 2, 2, 2, 2, 2, 2];
-        let value2 = [20, 20, 20, 20, 20, 20, 20, 20];
-
-        skip_list.insert(key1, value1);
-        skip_list.insert(key2, value2);
-
-        assert_eq!(skip_list.get(&key1), Some(value1));
-        assert_eq!(skip_list.get(&key2), Some(value2));
+        assert_eq!(skip_list.get(&[1]), Some(Box::from([10].to_vec())));
+        assert_eq!(skip_list.get(&[2]), Some(Box::from([20].to_vec())));
+        assert_eq!(skip_list.get(&[3]), Some(Box::from([30].to_vec())));
     }
 
+    // todo infinite loop
     #[test]
     fn test_insert_and_delete() {
         let mut skip_list = SkipList::new(4);
 
-        let key1 = [1, 1, 1, 1, 1, 1, 1, 1];
-        let value1 = [10, 10, 10, 10, 10, 10, 10, 10];
+        skip_list.insert(&[1], &[10]);
+        skip_list.insert(&[2], &[20]);
+        skip_list.insert(&[3], &[30]);
 
-        let key2 = [2, 2, 2, 2, 2, 2, 2, 2];
-        let value2 = [20, 20, 20, 20, 20, 20, 20, 20];
-
-        skip_list.insert(key1, value1);
-        skip_list.insert(key2, value2);
-
-        assert_eq!(skip_list.get(&key1), Some(value1));
-
-        let deleted_value = skip_list.delete(&key1);
-        assert_eq!(deleted_value, Some(value1));
-        assert_eq!(skip_list.get(&key1), None);
+        assert_eq!(skip_list.delete(&[2]), Some(Box::from([20])));
+        assert_eq!(skip_list.get(&[2]), None);
     }
 
     #[test]
-    fn test_insert_and_delete_nonexistent_key() {
+    fn test_insert_duplicate_key() {
         let mut skip_list = SkipList::new(4);
 
-        let key1 = [1, 1, 1, 1, 1, 1, 1, 1];
-        let value1 = [10, 10, 10, 10, 10, 10, 10, 10];
+        skip_list.insert(&[1], &[10]);
+        skip_list.insert(&[1], &[100]); // Duplicate key
 
-        let key2 = [2, 2, 2, 2, 2, 2, 2, 2];
-        let value2 = [20, 20, 20, 20, 20, 20, 20, 20];
-
-        skip_list.insert(key1, value1);
-
-        let deleted_value = skip_list.delete(&key2);
-        assert_eq!(deleted_value, None);
+        assert_eq!(skip_list.get(&[1]), Some(Box::from([100].to_vec())));
     }
 
+    #[test]
+    fn test_delete_nonexistent_key() {
+        let mut skip_list = SkipList::new(4);
+
+        skip_list.insert(&[1], &[10]);
+
+        assert_eq!(skip_list.delete(&[2]), None);
+    }
+
+    #[test]
+    fn test_empty_skiplist() {
+        let mut skip_list = SkipList::new(4);
+
+        assert_eq!(skip_list.get(&[1]), None);
+        assert_eq!(skip_list.delete(&[1]), None);
+    }
+
+    // todo fails
+    #[test]
+    fn test_random_insert_and_get() {
+        use rand::Rng;
+        let mut skip_list = SkipList::new(4);
+        let mut reference_map = std::collections::HashMap::new();
+        let mut rng = rand::thread_rng();
+
+        for _ in 0..1000 {
+            let key = rng.gen_range(0..1000);
+            let value = rng.gen_range(0..1000);
+            skip_list.insert(&[key as u8], &[value as u8]);
+            reference_map.insert(key, value);
+        }
+
+        for (key, value) in reference_map.iter() {
+            assert_eq!(skip_list.get(&[*key as u8]), Some(Box::from(vec![*value as u8])));
+        }
+    }
+
+    // todo infinite loop
 	#[test]
 	fn random_method_calls(){
-		let mut skip_list = SkipList::new(4);
-		assert_eq!(skip_list.insert([0,1,2,3,4,5,6,7], [0,0,11,11,1,1,1,1]), None); 
-		assert_eq!(skip_list.insert([0,2,2,3,4,5,6,7], [0,4,11,11,1,1,1,1]), None);
-		assert_eq!(skip_list.insert([0,3,2,3,4,5,6,7], [0,5,11,11,1,1,1,1]), None);
+		let mut skip_list = SkipList::new(10);
+		assert_eq!(skip_list.insert(&[0,1,2,3,4,5,6,7], &[0,0,11,11,1,1,1,1]), None);
+		assert_eq!(skip_list.insert(&[0,2,2,3,4,5,6,7], &[0,4,11,11,1,1,1,1]), None);
+		assert_eq!(skip_list.insert(&[0,3,2,3,4,5,6,7], &[0,5,11,11,1,1,1,1]), None);
 		assert_eq!(skip_list.delete(&[0,3,22,3,4,5,6,7]), None);
-		assert_eq!(skip_list.delete(&[0,3,2,3,4,5,6,7]), Some([0,5,11,11,1,1,1,1]));
-		assert_eq!(skip_list.insert([0,2,2,3,4,52,6,7], [0,4,11,111,1,12,1,1]), None);
-		assert_eq!(skip_list.insert([0,3,2,3,4,5,36,7], [0,5,11,11,1,31,1,1]), None);
-		assert_eq!(skip_list.delete(&[0,2,2,3,4,52,6,7]), Some([0,4,11,111,1,12,1,1]));
-		assert_eq!(skip_list.get(&[0,3,2,3,4,5,36,7]), Some([0,5,11,11,1,31,1,1]));
+		assert_eq!(skip_list.delete(&[0,3,2,3,4,5,6,7]), Some(Box::from([0,5,11,11,1,1,1,1].to_vec())));
+		assert_eq!(skip_list.insert(&[0,2,2,3,4,52,6,7], &[0,4,11,111,1,12,1,1]), None);
+		assert_eq!(skip_list.insert(&[0,3,2,3,4,5,36,7], &[0,5,11,11,1,31,1,1]), None);
+		assert_eq!(skip_list.delete(&[0,2,2,3,4,52,6,7]), Some(Box::from([0,4,11,111,1,12,1,1].to_vec())));
+		assert_eq!(skip_list.get(&[0,3,2,3,4,5,36,7]), Some(Box::from([0,5,11,11,1,31,1,1].to_vec())));
 
 		assert_eq!(skip_list.get(&[0,3,2,3,4,5,6,7]), None);
 	}
--- libraries/skip_list/src/skip_list.rs
+++ libraries/skip_list/src/skip_list.rs
@@ -1,48 +1,34 @@
 use std::rc::Rc;
-use std::cell:: { RefCell, Ref };
+use std::cell::RefCell;
+use std::cmp::Ordering;
 use rand::Rng;
-
 
 #[derive(Debug)]
 struct Node {
-	k: [u8; 8],
-	v: [u8; 8],
+	k: Option<Box<[u8]>>,
+	v: Option<Box<[u8]>>,
 	next: Vec<Link>,
 	level: usize,
-
 }
 
 impl Node {
-	pub fn new(k: [u8; 8], v: [u8; 8], l: usize, max_level: usize) -> Self {
+	pub fn new(k: Option<Box<[u8]>>, v: Option<Box<[u8]>>, level: usize, max_level: usize) -> Self {
 		Node {
 			k,
 			v,
 			next: vec![None; max_level],
-			level: l,
+			level
 		}
 	}
 
-	pub fn get_key(&self) -> &[u8; 8] {
-		&self.k
+	pub fn get_key(&self) -> &[u8] {
+		self.k.as_ref().unwrap()
 	}
 
-	pub fn get_val(&self) -> &[u8; 8] {
-		&self.v
-	}
-
-	pub fn set_level(&mut self, lvl: usize) {
-		self.level = lvl;
-	}
-
-	pub fn get_level(&self) -> usize {
-		self.level
-	}
-
-	pub fn set_value(&mut self, val: [u8;8]) {
-		self.v = val;
+	pub fn get_val(&self) -> &[u8] {
+		self.v.as_ref().unwrap()
 	}
 }
-
 
 pub struct SkipList {
 	tail: Rc<RefCell<Node>>,
@@ -52,10 +38,9 @@ pub struct SkipList {
 }
 
 impl SkipList {
-
 	pub fn new(max_level: usize) -> Self {
-		SkipList{
-			tail: Rc::new(RefCell::new(Node::new([0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], 0, max_level))),
+		SkipList {
+			tail: Rc::new(RefCell::new(Node::new(None, None, 0, max_level))),
 			level: 0,
 			max_level,
 			length: 0,
@@ -68,65 +53,61 @@ impl SkipList {
 		while rng.gen_range(0..=1) == 1 && level < self.max_level {
 			level += 1;
 		}
-	
+
 		level
 	}
 
-	pub fn get(&self, k: &[u8;8]) -> Option<[u8;8]> {
-
+	pub fn get(&self, key: &[u8]) -> Option<Box<[u8]>> {
 		let mut node = Rc::clone(&self.tail);
-		
-		for i in (0..self.level).rev() {
 
+		for i in (0..self.level).rev() {
 			while let Some(next) = &node.clone().borrow().next[i] {
 				let helper = next.borrow();
-				let key = helper.get_key();
-				if compare_bytes(key, k) == 0 {
-					return Some(helper.get_val().clone());
-				}
-				if compare_bytes(key, k) == 2 {
-					node = next.clone();
-				} else {
-					break;
+				let node_key = helper.get_key();
+
+				match node_key.cmp(key) {
+					Ordering::Less => break,
+					Ordering::Equal => return Some(Box::from(helper.get_val())),
+					Ordering::Greater => node = next.clone()
 				}
 			}
-
 		}
 
 		None
 	}
 
-	pub fn insert(&mut self, k: [u8;8], mut v: [u8;8]) -> Option<[u8;8]> {
-
+	pub fn insert(&mut self, key: &[u8], value: &[u8]) -> Option<Box<[u8]>> {
 		let mut node = Rc::clone(&self.tail);
 		let mut updates: Vec<Link> = vec![None; self.max_level];
 
 		for i in (0..self.level).rev() {
-
 			while let Some(next) = &Rc::clone(&node).borrow().next[i] {
 				let mut helper = next.borrow_mut();
-				let key = helper.get_key();
-				let value = helper.get_val().clone();
+				let node_key = helper.get_key();
 
-				if compare_bytes(key, &k) == 0 {
-					helper.v = v;
-					return Some(value);
-				}
-				if compare_bytes(key, &k) == 2 {
-					node = next.clone();
-				} else {
-					break;
+				match node_key.cmp(key) {
+					Ordering::Less => break,
+					Ordering::Equal => {
+						helper.v = Some(Box::from(value));
+						return Some(Box::from(helper.get_val()));
+					},
+					Ordering::Greater => node = next.clone()
 				}
 			}
 			updates[i] = Some(Rc::clone(&node));
 		}
 
 		let level = SkipList::random_gen(&self);
-		let node_to_insert = Rc::new(RefCell::new(Node::new(k, v, level, self.max_level)));
+		let node_to_insert = Rc::new(RefCell::new(Node::new(
+			Some(Box::from(key)),
+			Some(Box::from(value)),
+			level,
+			self.max_level
+		)));
 
 		if level > self.level {
-			for j in 0..level-self.level {
-				self.tail.borrow_mut().next[self.level+j] = Some(Rc::clone(&node_to_insert));
+			for j in 0..level - self.level {
+				self.tail.borrow_mut().next[self.level + j] = Some(Rc::clone(&node_to_insert));
 			}
 			self.level = level;
 		}
@@ -146,34 +127,29 @@ impl SkipList {
 
 		None
 	}
-	
 
-	pub fn delete(&mut self, k: &[u8;8]) -> Option<[u8;8]> {
-		
+	// todo infinite loop on test 'test_insert_and_delete' and test 'random_method_calls'
+	pub fn delete(&mut self, key: &[u8]) -> Option<Box<[u8]>> {
 		let mut node = Rc::clone(&self.tail);
 		let mut updates: Vec<Link> = vec![None; self.max_level];
 		let mut node_to_delete: Link = None;
 
 		for i in (0..self.level).rev() {
-
 			while let Some(next) = &Rc::clone(&node).borrow().next[i] {
 				let helper = next.borrow();
-				let key = helper.get_key();
-				
-				if compare_bytes(key, k) == 0 {
-					node_to_delete = Some(Rc::clone(&next));
-				}
-				if compare_bytes(key, k) == 2 {
-					node = next.clone();
-				} else {
-					break;
+				let node_key = helper.get_key();
+
+				match node_key.cmp(key) {
+					Ordering::Less => break,
+					Ordering::Equal => node_to_delete = Some(Rc::clone(&next)),
+					Ordering::Greater => node = next.clone()
 				}
 			}
 			updates[i] = Some(Rc::clone(&node));
 		}
 
 		if let Some(node_to_delete) = node_to_delete {
-			for (index, prev_node) in updates.iter().enumerate().take(node_to_delete.borrow().get_level()) {
+			for (index, prev_node) in updates.iter().enumerate().take(node_to_delete.borrow().level) {
 				if let Some(prev_node) = prev_node {
 					let next = &node_to_delete.borrow().next[index];
 					if next.is_some() {
@@ -182,33 +158,14 @@ impl SkipList {
 						prev_node.borrow_mut().next[index] = None;
 					}
 				}
-			} 
+			}
 
 			self.length -= 1;
 
-			return Some(*node_to_delete.borrow().get_val());
+			return Some(Box::from(node_to_delete.borrow().get_val()));
 		}
 		None
 	}
-
-	//TODO.. serialize & deserialize functions
-
-} 
-
-//TODO bolje iz [u8;8] dobiti usize::from_ne_bytes ali otom potom
-fn compare_bytes(first: &[u8; 8], second: &[u8; 8]) -> usize {
-
-	for i in (0..8).rev() {
-		if first[i] > second[i] {
-			return 1;
-		} else if second[i] > first[i] {
-			return 2;
-		} else {
-			continue;
-		}
-	}
-	0
 }
-
 
 type Link = Option<Rc<RefCell<Node>>>;
