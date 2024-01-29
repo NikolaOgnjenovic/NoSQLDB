e9b93fcf191c514162a29329db8b4be9f6bfe278
Momir Stanišić <momir.stanisic.ns@gmail.com>
2024-01-29 21:51:39 +0100

    Added variable encoding

--- libraries/compression/src/compression.rs
+++ libraries/compression/src/compression.rs
@@ -1,5 +1,9 @@
-use std::collections::HashMap;
+use bitvec::macros::internal::funty::Fundamental;
 use std::io::{Error, ErrorKind, Read, Write};
+use bitvec::prelude::{BitArray, Msb0};
+use std::collections::HashMap;
+use bitvec::view::BitView;
+use bitvec::vec::BitVec;
 use std::fs::File;
 
 
@@ -30,11 +34,11 @@ impl CompressionDictionary {
         file.read_to_end(&mut buffer)?;
 
         while file_cursor < buffer.len() {
-            let length = buffer[file_cursor] as usize;
-            file_cursor += 1;
+            let (key_length, offset) = variable_decode(&buffer[file_cursor..]);
+            file_cursor += offset;
 
-            let key_decoded: Box<[u8]> = Box::from(&buffer[file_cursor..file_cursor + length]);
-            file_cursor += length;
+            let key_decoded: Box<[u8]> = Box::from(&buffer[file_cursor..file_cursor + key_length.unwrap() as usize]);
+            file_cursor += key_length.unwrap() as usize;
             if !map.contains_key(&key_decoded) {
                 map.insert(key_decoded.clone(), list.len() as u64);
                 list.push(key_decoded);
@@ -54,7 +58,7 @@ impl CompressionDictionary {
             if !self.map.contains_key(key_decoded) {
                 self.map.insert(key_decoded.clone(), self.list.len() as u64);
                 self.list.push(key_decoded.clone());
-                buffer.push(key_decoded.len() as u8);
+                buffer.extend_from_slice(variable_encode(key_decoded.len() as u128).as_ref());
                 buffer.extend_from_slice(key_decoded);
             }
         }
@@ -72,7 +76,7 @@ impl CompressionDictionary {
                 self.map.insert(key.clone(), self.list.len() as u64);
                 self.list.push(key.clone());
                 let mut buffer = Vec::new();
-                buffer.push(key.len() as u8);
+                buffer.extend_from_slice(variable_encode(key.len() as u128).as_ref());
                 buffer.extend_from_slice(key);
                 self.file.write_all(&buffer)?;
                 Ok(Box::from((self.list.len() as u64).to_ne_bytes()))
@@ -93,5 +97,65 @@ impl CompressionDictionary {
             Some(key_decoded) => Ok(key_decoded.clone()),
             None => Err(Error::new(ErrorKind::InvalidData, "Encoded key is not in the dictionary!"))
         }
+    }
+}
+
+/// For given `number_value` returns the boxed slice of bytes with encoded value.
+pub fn variable_encode(number_value: u128) -> Box<[u8]> {
+    let mut bit_array = BitArray::<[u8; 16], Msb0>::default();
+    let mut buffer: Vec<u8> = vec![];
+
+    for i in 0i16..128 {
+        bit_array.set(127 - i as usize, (number_value >> i) & 1 == 1);
+    }
+
+    let leading_zeros = bit_array.leading_zeros();
+    let significant_bytes = if leading_zeros == 128 { 7 } else { 128 - leading_zeros };
+    let remainder_bytes = if significant_bytes % 7 != 0 { 7 - significant_bytes % 7 } else { 0 };
+    let start_index = 128 - significant_bytes - remainder_bytes;
+    let chunks = &mut bit_array[start_index..128].chunks(7).peekable();
+
+    while let Some(chunk) = chunks.next() {
+        let mut byte = 0u8;
+        for (i, bit) in chunk.iter().enumerate() {
+            if !bit.as_bool() {
+                byte |= 1 << (6 - i);
+            }
+        }
+        if chunks.peek().is_some() {
+            byte |= 1 << 7;
+        }
+        buffer.push(byte);
+    }
+
+    return buffer.into_boxed_slice();
+}
+
+/// For given `buffer` returns the first encoded value, and it's encoded length in bytes representing new offset.
+pub fn variable_decode(buffer: &[u8]) -> (Option<u128>, usize) {
+    let mut offset = 0;
+    let mut bits = BitVec::<u8, Msb0>::new();
+
+    for byte in buffer {
+        let slice = byte.view_bits::<Msb0>();
+        for bit in &slice[1..] {
+            bits.push(!bit.as_bool());
+        }
+        offset += 1;
+        if !slice[0] {
+            break;
+        }
+    }
+
+    if bits.len() <= 128 {
+        let mut value = 0u128;
+        for (i, bit) in bits.iter().enumerate() {
+            if !bit.as_bool() {
+                value |= 1 << (bits.len() - 1 - i);
+            }
+        }
+        (Some(value), offset)
+    } else {
+        (None, offset)
     }
 }
